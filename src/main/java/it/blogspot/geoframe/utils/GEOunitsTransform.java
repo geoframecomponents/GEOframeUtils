a1e14136139e96aaae7c9607419484f79b9a20c6
ftt01 <dallatorre.daniele@gmail.com>
2017-10-21 12:47:20 -0600

    Added Imperial-SI lenght units transform both ways

--- src/main/java/it/blogspot/geoframe/utils/GEOunitsTransform.java
+++ src/main/java/it/blogspot/geoframe/utils/GEOunitsTransform.java
@@ -30,7 +30,7 @@ public class GEOunitsTransform {
         GEOchecks.isNull(inputValue); // precondition
 
         if (inputValue > 1) inputValue = normalizeToOne(inputValue);
-        return inputValue * Math.PI/4;
+        return inputValue * Math.PI / 4;
     }
 
     public static double normalizeToOne(final Double inputValue) {
@@ -102,4 +102,83 @@ public class GEOunitsTransform {
         return inputValue * metersOverHectares;
     }
 
+    /** Units transform from inches to millimiters.
+     * @param inches
+     * @return millimiters
+     * @brief Inches to millimiters
+     */
+    public static Double inches2millimiters(final Double inches) {
+        final double millimitersOverInches = 25.4;
+        return inches * millimitersOverInches;
+    }
+
+    /** Units transform from millimiters to inches.
+     * @param millimiters
+     * @return inches
+     * @brief Millimiters to inches
+     */
+    public static Double millimiters2inches(final Double millimiters) {
+        final double millimitersOverInches = 25.4;
+        return millimiters / millimitersOverInches;
+    }
+
+    /** Units transform from feet to meters.
+     * @param feet
+     * @return meters
+     * @brief Feet to meters
+     */
+    public static Double feet2meters(final Double feet) {
+        final double metersOverFeet = 0.3048;
+        return feet * metersOverFeet;
+    }
+
+    /** Units transform from meters to feet.
+     * @param meters
+     * @return feet
+     * @brief Meters to feet
+     */
+    public static Double meters2feet(final Double meters) {
+        final double metersOverFeet = 0.3048;
+        return meters / metersOverFeet;
+    }
+
+    /** Units transform from yards to meters.
+     * @param yards
+     * @return meters
+     * @brief Yards to meters
+     */
+    public static Double yards2meters(final Double yards) {
+        final double metersOverYards = 0.9144;
+        return yards * metersOverYards;
+    }
+
+    /** Units transform from meters to yards.
+     * @param meters
+     * @return yards
+     * @brief  Meters to yards
+     */
+    public static Double meters2yards(final Double meters) {
+        final double metersOverYards = 0.9144;
+        return meters / metersOverYards;
+    }
+
+    /** Units transform from miles to kilometers.
+     * @param miles
+     * @return kilometers
+     * @brief Miles to kilometers
+     */
+    public static Double miles2kilometers(final Double miles) {
+        final double kilometersOverMiles = 1.609344;
+        return miles * kilometersOverMiles;
+    }
+
+    /** Units transform from kilometers to miles.
+     * @param kilometers
+     * @return miles
+     * @brief Miles to kilometers
+     */
+    public static Double kilometers2miles(final Double kilometers) {
+        final double kilometersOverMiles = 1.609344;
+        return kilometers / kilometersOverMiles;
+    }
 }
